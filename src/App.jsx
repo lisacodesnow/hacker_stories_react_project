92b1293a0f50673faeaa5c5f097e2755ae454da4
Lisa T <llmt2007@aol.com>
2023-04-19 15:28:29 -0400

    Installed ESlint with npm and the VSC  extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 
-function getTitle(title){
-  return title
-}
+const title = "React"
 function App(){
   
   return(
     <div>
-      <h1> Hello {getTitle("Rainy")}</h1>
+      <h1> Hello World</h1>
       <label htmlFor='search'>Search:</label>
       <input id="search" type="text"></input>
     </div>
