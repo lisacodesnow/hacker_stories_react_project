2df24bfcc186a7da1c48160b0cdd617c1689bc04
Lisa T <llmt2007@aol.com>
2023-04-26 13:48:12 -0400

    Practicing rendering arrays in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
 import * as React from 'react';
 
-const title = "React"
+const list = [
+
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 function App(){
+  
+  const renderList = list.map( (item) =>(
+
+    <li key={item.objectID}>
+      <a href= {item.url}>{item.title}</a><br></br>
+      <span>{item.author}</span><br></br>
+      <span>{item.num_comments} </span><br></br>
+     <span> {item.points}</span>
+    </li>
+  )
+  )
+
   
   return(
     <div>
-      <h1> Hello World</h1>
+      <h1> My Hacker Stories</h1>
       <label htmlFor='search'>Search:</label>
       <input id="search" type="text"></input>
+
+      <hr/>
+      
+       {renderList}
+      
     </div>
   )
 }
